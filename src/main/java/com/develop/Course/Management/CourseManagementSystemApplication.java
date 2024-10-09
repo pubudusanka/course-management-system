f1a86b35a50fe1c0495756891c110ea19bb9fb76
pubudusanka <pubudusanka79@gmail.com>
2024-10-09 22:04:01 +0530

    Adding SpringBoot
    add springboot framework files to the repository

---
+++ src/main/java/com/develop/Course/Management/CourseManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.develop.Course.Management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseManagementSystemApplication.class, args);
+	}
+
+}
