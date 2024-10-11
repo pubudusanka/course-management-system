f662c8c4b1c45e99aeb15839554c0789c79e82a7
pubudusanka <pubudusanka79@gmail.com>
2024-10-11 23:44:06 +0530

    Update CourseController.java

--- src/main/java/com/develop/Course/Management/controller/CourseController.java
+++ src/main/java/com/develop/Course/Management/controller/CourseController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/courses")
-@CrossOrigin(origins = "http://localhost:5173")
+/*@CrossOrigin(origins = "http://localhost:5173")*/
 public class CourseController {
 
     @Autowired
