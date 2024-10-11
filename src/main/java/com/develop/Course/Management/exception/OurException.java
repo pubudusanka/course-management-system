47dffd5b0728212959b70ee39c0922ee0efc9745
pubudusanka <pubudusanka79@gmail.com>
2024-10-11 15:46:56 +0530

    Create OurException.java

---
+++ src/main/java/com/develop/Course/Management/exception/OurException.java
@@ -0,0 +1,8 @@
+package com.develop.Course.Management.exception;
+
+public class OurException extends RuntimeException{
+
+    public OurException(String message){
+        super(message);
+    }
+}
